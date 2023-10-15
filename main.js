a908e87ded00acd0b184c6bacf426623e4e4f6ba
javoh1rkhon <rustamovjavohirkhon@gmail.com>
2023-10-15 12:06:34 +0500

    number methods

---
+++ main.js
@@ -0,0 +1,68 @@
+// let name = "jhon"
+// let admin = name 
+
+// console.log(admin);
+
+// //balans
+// let balance = 25000
+
+// //xarajat
+// let car = 4480
+// let food = 890
+
+// let xarajat = car + food
+
+// //foyda
+// let inovoice = 5500
+// let stock = 4200
+
+// let foyda = inovoice + stock 
+
+// let totalbalance = 0
+
+// totalbalance = (balance - xarajat) + foyda
+
+// console.log(totalbalance)
+
+
+
+// mavzu number methods
+
+
+// let age = "24.1"
+
+// let a = parseInt(age)
+// let b = parseFloat(ag    e)
+
+// let y = Math.floor(b)
+// let u = Math.ceil(b)
+// let t = Math.round(b)
+
+// console.log(y);
+// console.log(u);
+// console.log(t)
+
+
+// let rnd = Math.round(Math.random() * 100)
+
+// console.log(rnd);
+
+let a = 10
+let b = 15
+let c = 20
+
+let max = Math.max(a, b, c)
+let min = Math.min(a, b, c)
+// console.log(max);
+// console.log(min);
+
+let y = Math.pow(a, b)
+
+console.log(y  );
+
+// console.log(
+//     typeof(age),
+//     typeof(a),
+//     typeof(b),
+//     a, b
+// );
